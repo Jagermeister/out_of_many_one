37e66df28bf1c7bda45349cd4709756baeeae9d0
MatthewYeager <MatthewBrandonYeager+git@gmail.com>
2019-05-29 14:53:45 -0700

    Adding SQL Create/Read

--- src/store/sql.py
+++ src/store/sql.py
@@ -50,6 +50,23 @@ FILER_TABLE_CREATE = '''
     );
 '''
 
+FILER_CREATE = '''
+    INSERT INTO filer (
+        name_first,
+        name_last
+    ) VALUES (
+        ?, ?
+    );
+'''
+
+FILERS_READ = '''
+    SELECT
+        F.filer_key,
+        F.name_first,
+        F.name_last
+    FROM filer AS F;
+'''
+
 
 ### Filer Type
 
@@ -61,6 +78,29 @@ FILER_TYPE_TABLE_CREATE = '''
         is_candidate INTEGER NOT NULL,
         is_former_senator INTEGER NOT NULL
     );
+'''
+
+FILER_TYPES_READ = '''
+    SELECT
+        FT.filer_key,
+        FT.filer_name,
+        FT.is_senator,
+        FT.is_candidate,
+        FT.is_former_senator
+    FROM filer_type AS FT;
+'''
+
+FILTER_TYPE_POPULATE = '''
+    INSERT INTO filer_type (
+        filer_name,
+        is_senator,
+        is_candidate,
+        is_former_senator
+    )
+    VALUES
+        ('Senator', 1, 0, 0),
+        ('Candidate', 0, 1, 0),
+        ('Former Senator', 0, 0, 1);
 '''
 
 
@@ -75,29 +115,72 @@ DOCUMENT_TYPE_TABLE_CREATE = '''
     );
 '''
 
+DOCUMENT_TYPE_POPULATE = '''
+    INSERT INTO document_type (
+        document_type_name,
+        is_electronic,
+        is_paper
+    )
+    VALES
+        ('Electronic', 1, 0),
+        ('Paper', 0, 1);
+'''
+
+DOCUMENT_TYPES_READ = '''
+    SELECT
+        DT.document_type_key,
+        DT.document_type_name,
+        DT.is_electronic,
+        DT.is_paper
+    FROM document_type AS DT;
+'''
+
 
 ### Document
 
 DOCUMENT_TABLE_CREATE = '''
     CREATE TABLE IF NOT EXISTS document (
         document_key INTEGER PRIMARY KEY,
+        report_key INTEGER NOT NULL,
         filer_key INTEGER NOT NULL,
-        filter_type_key INTEGER NOT NULL,
+        filer_type_key INTEGER NOT NULL,
         document_type_key INTEGER NOT NULL,
         unique_id TEXT NOT NULL,
         document_name TEXT,
         document_year INTEGER,
         document_date INTEGER,
+        FOREIGN KEY(report_key) REFERENCES report(report_key),
         FOREIGN KEY(filer_key) REFERENCES filer(filer_key),
-        FOREIGN KEY(filter_type_key) REFERENCES filer_type(filter_type_key),
+        FOREIGN KEY(filer_type_key) REFERENCES filer_type(filer_type_key),
         FOREIGN KEY(document_type_key) REFERENCES document_type(document_type_key)
     );
 '''
 
-TABLE_CREATIONS = [
+DOCUMENT_CREATE = '''
+    INSERT INTO document (
+        report_key,
+        filer_key,
+        filer_type_key,
+        document_type_key,
+        unique_id,
+        document_name,
+        document_year,
+        document_date
+    ) VALUES (
+        ?, ?, ?, ?, ?, ?, ?, ?
+    )
+'''
+
+
+TABLES_CREATION = [
     DOCUMENT_TABLE_CREATE,
     DOCUMENT_TYPE_TABLE_CREATE,
     FILER_TABLE_CREATE,
     FILER_TYPE_TABLE_CREATE,
     REPORT_TABLE_CREATE
+]
+
+TABLES_POPULATE_DATA = [
+    DOCUMENT_TYPE_POPULATE,
+    FILTER_TYPE_POPULATE
 ]
--- src/store/storage.py
+++ src/store/storage.py
@@ -3,7 +3,13 @@
 import sqlite3
 
 from src.store.sql import (
-    TABLE_CREATIONS,
+    DOCUMENT_CREATE,
+    DOCUMENT_TYPES_READ,
+    FILER_CREATE,
+    FILERS_READ,
+    FILER_TYPES_READ,
+    TABLES_CREATION,
+    TABLES_POPULATE_DATA,
     REPORT_CREATE,
     REPORTS_READ
 )
@@ -25,11 +31,50 @@ class Storage():
         """ Close all open connections """
         self.connection.close()
 
-    def database_tables_create(self):
+    def database_tables_create_and_populate(self):
         """ Create all tables not existing within the database """
-        for table in TABLE_CREATIONS:
+        for table in TABLES_CREATION:
             self.cursor.execute(table)
             self.save()
+
+        for table in TABLES_POPULATE_DATA:
+            self.cursor.execute(table)
+            self.save()
+
+    def document_add(self, document):
+        """ Add document to storage
+        Args:
+            document: tuple or list(tuple) - report_key, filer_key,
+                filer_type_key, document_type_key, unique_id,
+                document_name, document_year, document_date
+        """
+        command = self.cursor.executemany if isinstance(document, list) else self.cursor.execute
+        command(DOCUMENT_CREATE, document)
+        self.save()
+
+    def document_types_get(self):
+        """ Select all document_types """
+        self.cursor.execute(DOCUMENT_TYPES_READ)
+        return self.cursor.fetchall()
+
+    def filer_add(self, filer):
+        """ Add filer to storage
+        Args:
+            filer: tuple or list(tuple) - name_first, name_last
+        """
+        command = self.cursor.executemany if isinstance(filer, list) else self.cursor.execute
+        command(FILER_CREATE, filer)
+        self.save()
+
+    def filers_get(self):
+        """ Select all filers """
+        self.cursor.execute(FILERS_READ)
+        return self.cursor.fetchall()
+
+    def filer_types_get(self):
+        """ Select all filer_types """
+        self.cursor.execute(FILER_TYPES_READ)
+        return self.cursor.fetchall()
 
     def report_add(self, report):
         """ Add raw report details
